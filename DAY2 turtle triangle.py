9980951f1f82460a1444abfa45685e3e7ab30fb1
cleanandsimple <81221409+cleanandsimple@users.noreply.github.com>
2021-04-01 23:54:24 +0900

    DAY2

---
+++ DAY2 turtle triangle.py
@@ -0,0 +1,10 @@
+#삼각형 만들기는 많은 경우의 수가 있지만 반복문을 쓰기에 적합한 형태를 미리 학습해 둡시다.
+
+import turtle as t
+a=120
+b=100
+for x in range (3) :
+    t.fd(b)
+    t.lt(a)
+
+
---
+++ DAY2 turtle.square.py
@@ -0,0 +1,8 @@
+#이번에는 사각형을 그려보겠읍니다.
+import turtle as t
+a=100
+b=90
+for x in range (4) :
+    t.fd(a)
+    t.lt(b)
+    
